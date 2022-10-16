2daf3c269514f5bb58d73ecf7954d77905b156fe
DivyaTanwar <106358895+DivyaTanwar@users.noreply.github.com>
2022-10-16 01:05:12 -0400

    new file:   Program.cs

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿class TestClass
+{
+    static void Main(string[] args)
+    {
+        double amount, total_amount, tax;
+        Console.WriteLine("Enter the minutes used:");
+        string? input = Console.ReadLine();
+
+        int units = Convert.ToInt32(input);
+
+        switch (units <= 200)
+        {
+            case true:
+                amount = units * 0.25;
+                break;
+            case false:
+            // units between 201 - 500
+                switch (units <= 500)
+                {
+                    case true:
+                        amount = 50 + (units - 200) * 0.45;
+                        break;
+                    case false:
+                    // units > 500
+                        amount = 50 + 135;
+                        break;
+                }
+                break;
+        }
+        tax = amount * 0.13;
+        total_amount = amount + tax;
+        // cout << "Your total bill is $" << total_amount;
+        Console.WriteLine("Total tax: {0}", tax);
+        Console.WriteLine("Total charge: {0}", total_amount);
+    }
+}
